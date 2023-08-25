60ee1e162c92467e5e8a9610781631f18f24bc9f
prathyusha <prathyucv@gmail.com>
2023-08-25 23:20:09 +0530

    ReCaptcha added

--- Script.js
+++ Script.js
@@ -1,7 +1,7 @@
 
 function beforeSubmit(){
- let inputDate = document.querySelector('.inputdate');
- let outputDate = document.querySelector('.outputdate');//string to Date(en_IN) form
+let inputDate = document.querySelector('.inputdate');
+let outputDate = document.querySelector('.outputdate');//string to Date(en_IN) form
 
 console.log('input date::' , inputDate.value);
 let formattedDate = new Date(inputDate.value).toLocaleDateString("en-IN");
@@ -9,3 +9,12 @@ console.log(typeof formattedDate,formattedDate);
 outputDate.value = formattedDate;
 console.log(outputDate.value);
 }
+
+function timestamp() { var response = document.getElementById("g-recaptcha-response");
+ if (response == null || response.value.trim() == "") {
+    var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);
+    elems["ts"] = JSON.stringify(new Date().getTime());
+    document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); 
+ } 
+} 
+setInterval(timestamp, 500); 
