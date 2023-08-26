9806e41c19265fd290c17bd3c94557f588e88791
prathyusha <prathyucv@gmail.com>
2023-08-26 13:06:36 +0530

    Locale changes

--- Script.js
+++ Script.js
@@ -1,14 +1,14 @@
 let captchaChecked = false;
 function beforeSubmit(event){
     if(captchaChecked){
+        let outputDate = document.querySelector('.outputdate');
         let inputDate = document.querySelector('.inputdate');
-        let outputDate = document.querySelector('.outputdate');//string to Date(en_IN) form
-
-        console.log('input date::' , inputDate.value);
-        let formattedDate = new Date(inputDate.value).toLocaleDateString("en-IN");
+        
+        console.log('input date::' , inputDate.value);//string to Date(en_US) form
+        let formattedDate = new Date(inputDate.value).toLocaleDateString("en-US");
         console.log(typeof formattedDate,formattedDate);
         outputDate.value = formattedDate;
-        console.log(outputDate.value);
+        console.log(typeof outputDate.value, outputDate.value);
     }else{
         alert("Please select reCAPTCHA checkbox to submit the Lead.");
         event.preventDefault();
